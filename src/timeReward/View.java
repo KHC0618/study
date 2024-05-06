483625940c1d2d2bc10d804d38698d323f308140
KimHeeCheol <audwkd0618@naver.com>
2024-05-06 23:51:24 +0900

    20240506 commit

--- src/timeReward/View.java
+++ src/timeReward/View.java
@@ -5,6 +5,10 @@ import java.awt.*;
 
 public class View extends JFrame {
     public View() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
+        setSize(200, 100);
 
+        setVisible(true);
     }
 }
